f08909a4c16f5f3335b6200f274bd2ffbd60d662
GaneshIT <ganesh.ceoit@gmail.com>
2025-09-17 14:07:19 +0530

    updated

--- js/movie.js
+++ js/movie.js
@@ -1,3 +1,10 @@
+var date=new Date(); //To get current date and time
+document.getElementById('displayTime').innerText=date.toLocaleTimeString();
+setInterval(function(){
+date=new Date();
+document.getElementById('displayTime').innerText=date.toLocaleTimeString();
+},1000);//1000 milliseconds = 1 second
+
 function validateMovieForm() {
     var mname= document.getElementById('txtmname').value;
     var mdesc= document.getElementById('txtmdesc').value;
@@ -85,16 +92,17 @@ function addMovie(){
 function showMovies(){
     var rows='';
     for(let i=0;i<mnames.length;i++){
-        rows=rows+'<tr><td>'+mnames[i]+'</td><td>'+mdescs[i]+'</td><td>'+mtypes[i]+'</td><td><button onclick="editMovie('+i+')">Edit</button></td><td><button>Delete</button></td></tr>'
+        rows=rows+'<tr><td>'+mnames[i]+'</td><td>'+mdescs[i]+'</td><td>'+mtypes[i]+'</td><td><button onclick="editMovie('+i+')">Edit</button></td><td><button onclick="deleteMovie('+i+')">Delete</button></td></tr>'
     }
     document.getElementById('movietable').innerHTML=rows;
 }
-
-function editMovie(index){
-    console.log('Edit movie at index:',index);//Edit Movie at index: 1
-    document.getElementById('txtmname').value = mnames[index];//[0] a,[1] x
-    document.getElementById('txtmdesc').value = mdescs[index];
-    document.getElementById('ddltype').value = mtypes[index];
+var index=0;//global variable to access in multiple functions
+function editMovie(idx){
+    index=idx;//assigning local variable value to global variable
+    console.log('Edit movie at index:',idx);//Edit Movie at index: 1
+    document.getElementById('txtmname').value = mnames[idx];//[0] a,[1] x
+    document.getElementById('txtmdesc').value = mdescs[idx];
+    document.getElementById('ddltype').value = mtypes[idx];
 
     //hide save button, show update button
     document.getElementById('btnsave').style.display='none';//hide save button
@@ -102,4 +110,28 @@ function editMovie(index){
 }
 function deleteMovie(index){
     console.log('Delete movie at index:',index);
+    mnames.splice(index,1);//1 means delete 1 item at given index
+    mdescs.splice(index,1);
+    mtypes.splice(index,1);
+    showMovies();
+}
+function updateMovie(){
+    var mname= document.getElementById('txtmname').value;
+    var mtype= document.getElementById('ddltype').value;
+    var mdesc= document.getElementById('txtmdesc').value;
+    //update array values
+    mnames[index]=mname;
+    mdescs[index]=mdesc;
+    mtypes[index]=mtype;
+    showMovies();//it will read updated array values and display in table
+    resetForm();
+}
+//reset form
+function resetForm(){
+    document.getElementById('txtmname').value='';
+    document.getElementById('txtmdesc').value='';
+    document.getElementById('ddltype').value='Select';
+    //show save button, hide update button
+    document.getElementById('btnsave').style.display='block';//show save button
+    document.getElementById('btnupdate').style.display='none';//hide update button
 }
